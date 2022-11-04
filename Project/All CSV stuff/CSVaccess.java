fde5e7b080ade17541bb5851ad0f198a24108e54
a9fe4465891fc <kande30@illinois.edu>
2022-11-03 23:39:20 -0500

    x
    x

---
+++ Project/All CSV stuff/CSVaccess.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadCSVFile {
+    public static void main(String[]  args) {
+        public void test() {
+            objReader = new BufferedReader(new FileReader("Project/All CSV stuff/test.txt"));
+        }
+    }
+} 
